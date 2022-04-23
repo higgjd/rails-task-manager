9ca74d689d8e3452fefb8f73ccbb1da2db432758
Jenny Higgins <higgjd@gmail.com>
2022-04-23 15:34:15 +0100

    completed rails task manager challenge - created app with all CRUD functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :tasks
+  # get 'tasks', to: 'tasks#index'
+  # get 'task/new', to: 'tasks#new'
+  # get 'task/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'task/:id', to: 'tasks#update'
+  # delete 'task/:id', to: 'tasks#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
